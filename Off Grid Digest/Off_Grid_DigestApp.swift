c33f59e0918e51d4e29c65889c1daf537aeae51a
Josh Daniel <jdaniel@crosslinktax.com>
2025-09-10 18:39:31 -0700

    Initial Commit

---
+++ Off Grid Digest/Off_Grid_DigestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Off_Grid_DigestApp.swift
+//  Off Grid Digest
+//
+//  Created by Josh Daniel on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Off_Grid_DigestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
